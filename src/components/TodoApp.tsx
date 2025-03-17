e8267cd1866c96aba473a9573837f62df6c418cb
VictorBartolomeo <victor.monteriggioni@gmail.com>
2025-03-17 22:56:48 +0100

    refacto todo app component

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -1,67 +1,55 @@
-type TodoItem = {
-    id: number,
-    description: string
-    done: boolean
-}
+// TodoApp.tsx
+import React, { useState } from 'react';
+import { SearchBar } from './SearchBar';
+import { TodoItem } from './TodoItem';
+import { TodoItemType } from './types'; // Import the type
+
 
 export function TodoApp() {
+    const [todos, setTodos] = useState<TodoItemType[]>([
+        { id: 1, description: 'Acheter des oranges', done: false },
+        { id: 2, description: 'Courir avec le fraté', done: true },
+        { id: 3, description: 'Me faire défoncer à LoL', done: true },
+    ]);
+
+    const handleAddTodo = (description: string) => {
+        const newTodo: TodoItemType = {
+            id: Date.now(), // Simple unique ID for now
+            description,
+            done: false,
+        };
+        setTodos([...todos, newTodo]);
+    };
+
+    const handleToggleDone = (id: number) => {
+        setTodos(
+            todos.map((todo) =>
+                todo.id === id ? { ...todo, done: !todo.done } : todo
+            )
+        );
+    };
+
+    const handleDeleteTodo = (id: number) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    };
+
+    // Sort todos: unfinished first, then finished
+    const sortedTodos = [...todos].sort((a, b) => (a.done === b.done ? 0 : a.done ? 1 : -1));
+
     return (
         <>
-            <div className="flex">
-
-                <label className="input input-bordered flex items-center gap-2">
-                    <input type="text" className="grow" placeholder="Ajouter une tâche" />
-                </label>
-
-                <button className="btn btn-primary">+</button>
-
-            </div>
+            <SearchBar onAddTodo={handleAddTodo} />
 
             <div className="my-5 flex-column gap-5 w-full text-left">
-                {/* TODO ITEM version normal */}
-                <div className="bg-indigo-700 w-full m-5 rounded-box p-3 flex">
-                    <span className="pr-8">
-                        <input type="checkbox" className="checkbox" />
-                    </span>
-                    <span className="flex-grow">
-                        Acheter des oranges
-                    </span>
-                    <div>
-                        <button className="btn btn-error btn-outline btn-xs">
-                            X
-                        </button>
-                    </div>
-                </div>
-
-                {/* TODO Item version cochée */}
-                <div className="bg-indigo-900 w-full m-5 rounded-box p-3 flex">
-                    <span className="pr-8">
-                        <input type="checkbox" checked={true} className="checkbox" />
-                    </span>
-                    <span className="flex-grow line-through">
-                        Courir avec le fraté
-                    </span>
-                    <div>
-                        <button className="btn btn-error btn-outline btn-xs">
-                            X
-                        </button>
-                    </div>
-                </div>
-
-                <div className="bg-indigo-900 w-full m-5 rounded-box p-3 flex">
-                    <span className="pr-8">
-                        <input type="checkbox" checked={true} className="checkbox" />
-                    </span>
-                    <span className="flex-grow line-through">
-                        Me faire défoncer à LoL
-                    </span>
-                    <div>
-                        <button className="btn btn-error btn-outline btn-xs">
-                            X
-                        </button>
-                    </div>
-                </div>
+                {sortedTodos.map((todo) => (
+                    <TodoItem
+                        key={todo.id}
+                        todo={todo}
+                        onToggleDone={handleToggleDone}
+                        onDeleteTodo={handleDeleteTodo}
+                    />
+                ))}
             </div>
         </>
-    )
+    );
 }
